d48ac4cea4348eddba9a2b0c842891bb52c400d5
Dotinkasra <tyekepon@pm.me>
2026-02-06 12:14:43 +0900

    リファクタリング

--- components/result_table.py
+++ components/result_table.py
@@ -1,6 +1,7 @@
 import flet as ft
 
-from module.data import User, Hule, Bonus
+from module.data import Bonus, Hule, User
+
 
 class ResultTablePage(ft.UserControl):
 
@@ -9,7 +10,8 @@ class ResultTablePage(ft.UserControl):
         self.user = user
 
     def did_mount(self):
-        self.page.update()
+        if self.page:
+            self.page.update()
 
     def build(self):
         user = self.user
@@ -33,26 +35,28 @@ class ResultTablePage(ft.UserControl):
                         ft.DataCell(ft.Text(f"{user.ura_dora}回")),
                         ft.DataCell(ft.Text(f"{user.ippatsu}回")),
                         ft.DataCell(ft.Text(f"{user.allstar}回")),
-                        ft.DataCell(ft.Text(f"{user.yiman}回")), 
+                        ft.DataCell(ft.Text(f"{user.yiman}回")),
                         ft.DataCell(ft.Text(f"{user.tobi}回")),
                     ],
-                ),ft.DataRow(
+                ),
+                ft.DataRow(
                     cells=[
                         ft.DataCell(ft.Text(f"{user.aka_dora_tumo}回")),
                         ft.DataCell(ft.Text(f"{user.ura_dora_tumo}回")),
                         ft.DataCell(ft.Text(f"{user.ippatsu_tumo}回")),
                         ft.DataCell(ft.Text(f"{user.allstar_tumo}回")),
-                        ft.DataCell(ft.Text(f"{user.yiman_tumo}回")), 
-                        ft.DataCell(ft.Text(f"-")), 
+                        ft.DataCell(ft.Text(f"{user.yiman_tumo}回")),
+                        ft.DataCell(ft.Text(f"-")),
                     ],
-                ),ft.DataRow(
+                ),
+                ft.DataRow(
                     cells=[
                         ft.DataCell(ft.Text("")),
                         ft.DataCell(ft.Text("")),
                         ft.DataCell(ft.Text("")),
                         ft.DataCell(ft.Text("")),
                         ft.DataCell(ft.Text("合計")),
-                        ft.DataCell(ft.Text(f"{user.bonus_yen}円")), 
+                        ft.DataCell(ft.Text(f"{user.bonus_yen}円")),
                     ],
                 ),
             ],
@@ -63,23 +67,25 @@ class ResultTablePage(ft.UserControl):
         if len(user.transaction) <= 0:
             less_rows.append(
                 ft.DataRow(
-                        cells=[
-                            ft.DataCell(ft.Text("なし")),
-                            ft.DataCell(ft.Text("")),
-                            ft.DataCell(ft.Text("0円")),
-                        ]
+                    cells=[
+                        ft.DataCell(ft.Text("なし")),
+                        ft.DataCell(ft.Text("")),
+                        ft.DataCell(ft.Text("0円")),
+                    ]
                 )
             )
         else:
             for hule in user.transaction:
                 less_rows.append(
                     ft.DataRow(
-                            cells=[
-                                ft.DataCell(ft.Text(hule.to)),
-                                ft.DataCell(ft.Text(f"{hule.bonus.value}(ツモ)" if hule.zimo else f"{hule.bonus.value}(ロン)")),
-                                ft.DataCell(ft.Text(f"{hule.yen}円")),
-                            ],
-                        )
+                        cells=[
+                            ft.DataCell(ft.Text(hule.to)),
+                            ft.DataCell(
+                                ft.Text(f"{hule.bonus.value}(ツモ)" if hule.zimo else f"{hule.bonus.value}(ロン)")
+                            ),
+                            ft.DataCell(ft.Text(f"{hule.yen}円")),
+                        ],
+                    )
                 )
                 less_total += hule.yen
 
@@ -93,9 +99,9 @@ class ResultTablePage(ft.UserControl):
                 ft.DataColumn(ft.Text("内容")),
                 ft.DataColumn(ft.Text("金額")),
             ],
-            rows=less_rows
+            rows=less_rows,
         )
-        
+
         bonus_total = user.bonus_yen - less_total
         result = user.score_yen + bonus_total
 
@@ -105,8 +111,8 @@ class ResultTablePage(ft.UserControl):
             horizontal_lines=ft.BorderSide(1, "black"),
             border=ft.border.all(2, "black"),
             columns=[
-                ft.DataColumn(ft.Text("収入")),
-                ft.DataColumn(ft.Text("支出")),
+                ft.DataColumn(ft.Text("得点")),
+                ft.DataColumn(ft.Text("祝儀")),
                 ft.DataColumn(ft.Text("結果")),
             ],
             rows=[
@@ -114,45 +120,69 @@ class ResultTablePage(ft.UserControl):
                     cells=[
                         ft.DataCell(ft.Text(f"{user.score_yen}円")),
                         ft.DataCell(ft.Text(f"{bonus_total}円")),
-                        ft.DataCell(ft.Text(f"{result}円", color = ft.Colors.RED if result < 0 else ft.Colors.GREEN_900 )),
+                        ft.DataCell(ft.Text(f"{result}円", color=ft.Colors.RED if result < 0 else ft.Colors.GREEN_900)),
                     ],
                 ),
             ],
         )
-#        return [bonus, user_less_table, user_result_table]
-        container = ft.Column([
-            ft.Container(
-                ft.Column([
-                    ft.Row([
-                        ft.Text(f"【{user.nickname}】", theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM),
-                        ft.Text(user.point, theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM,),
-                        ft.Text(f"({user.score})", theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM, color = ft.Colors.RED if user.score < 0 else ft.Colors.GREEN)
-                        ], alignment=ft.MainAxisAlignment.START
-                        
+        container = ft.Column(
+            [
+                ft.Container(
+                    ft.Column(
+                        [
+                            ft.Row(
+                                [
+                                    ft.Text(f"【{user.nickname}】", theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM),
+                                    ft.Text(
+                                        str(user.point),
+                                        theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM,
+                                    ),
+                                    ft.Text(
+                                        f"({user.score})",
+                                        theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM,
+                                        color=ft.Colors.RED if user.score < 0 else ft.Colors.GREEN,
+                                    ),
+                                ],
+                                alignment=ft.MainAxisAlignment.START,
+                            ),
+                            ft.Row(
+                                [
+                                    self.result_table,
+                                ],
+                                alignment=ft.MainAxisAlignment.START,
+                            ),
+                            ft.Row(
+                                [
+                                    ft.Text(
+                                        f"祝儀明細（上段：ロン、下段：ツモ）", theme_style=ft.TextThemeStyle.BODY_LARGE
+                                    ),
+                                ],
+                                alignment=ft.MainAxisAlignment.START,
+                            ),
+                            ft.Row(
+                                [
+                                    self.bonus_table,
+                                ],
+                                alignment=ft.MainAxisAlignment.START,
+                            ),
+                            ft.Row(
+                                [
+                                    ft.Text(f"支払い明細", theme_style=ft.TextThemeStyle.BODY_LARGE),
+                                ],
+                                alignment=ft.MainAxisAlignment.START,
+                            ),
+                            ft.Row(
+                                [
+                                    self.less_table,
+                                ],
+                                alignment=ft.MainAxisAlignment.START,
+                            ),
+                        ]
                     ),
-                    ft.Row([
-                        self.result_table,
-                    ], alignment=ft.MainAxisAlignment.START),
-                    ft.Row([
-                        ft.Text(f"祝儀明細（上段：ロン、下段：ツモ）", theme_style=ft.TextThemeStyle.BODY_LARGE),
-                        ], alignment=ft.MainAxisAlignment.START
-                    ),
-                    ft.Row([
-                        self.bonus_table,
-                    ], alignment=ft.MainAxisAlignment.START),
-                    ft.Row([
-                        ft.Text(f"支払い明細", theme_style=ft.TextThemeStyle.BODY_LARGE),
-                        ], alignment=ft.MainAxisAlignment.START
-                    ),
-                    ft.Row([
-                        self.less_table,
-                    ], alignment=ft.MainAxisAlignment.START),
-                    
-                    
-                ]),
-                margin=10,
-                padding=10,
-            )
-        ])
-        
+                    margin=10,
+                    padding=10,
+                )
+            ]
+        )
+
         return container
--- module/jansoul.py
+++ module/jansoul.py
@@ -1,9 +1,11 @@
 import json
+
 from module.data import Bonus, Hule, User
 
 
 class Jansoul:
     RATE = 500
+
     def __init__(self, paifu: dict, samma: bool = False):
         self.paifu = paifu
         self.users = self._create_users(samma=samma)
@@ -18,17 +20,17 @@ class Jansoul:
         Parameters
         ----------
         samma bool: サンマの場合はTrue, ヨンマの場合はFalse
-        
+
         Returns
         -------
         list[User]: 取得したUserのリスト
 
         """
         users = []
-        seats = [0, 1, 2]  if samma else [0, 1, 2, 3]
+        seats = [0, 1, 2] if samma else [0, 1, 2, 3]
         for u in self.paifu["head"]["accounts"]:
             user = User(seat=u["seat"], nickname=u["nickname"])
-            seats.remove(u['seat'])
+            seats.remove(u["seat"])
             users.append(user)
         if len(seats) > 0:
             for i, seat in enumerate(seats):
@@ -39,8 +41,8 @@ class Jansoul:
             user.point = point_dict[user.seat]
 
         return users
-    
-    def _get_user_with_seat(self, seat: int) -> User:
+
+    def _get_user_with_seat(self, seat: int) -> User | None:
         """シート番号のUserを取得する
 
         Parameters
@@ -50,7 +52,7 @@ class Jansoul:
         Returns
         -------
         User: シート番号に着席しているUser
-        
+
         """
         for u in self.users:
             if u.seat == seat:
@@ -62,52 +64,53 @@ class Jansoul:
         Returns
         -------
         list: 和了のデータ
-        
-        """        
+
+        """
         filtered_actions = [
-            action for action in self.paifu["data"]["data"]["actions"]
-            if action['type'] == 1 and action['result']['name'] == ".lq.RecordHule"
+            action
+            for action in self.paifu["data"]["data"]["actions"]
+            if action["type"] == 1 and action["result"]["name"] == ".lq.RecordHule"
         ]
         return filtered_actions
- 
+
     def _calc_bonus(self):
         def count_ippatsu(hule: dict) -> int:
-            for fans in hule['fans']:
-                if fans['id'] == 30:
-                    return fans['val']
+            for fans in hule["fans"]:
+                if fans["id"] == 30:
+                    return fans["val"]
             return 0
-                
+
         def count_aka_dora(hule: dict) -> int:
-            for fans in hule['fans']:
-                if fans['id'] == 32:
-                    return fans['val']
+            for fans in hule["fans"]:
+                if fans["id"] == 32:
+                    return fans["val"]
             return 0
-        
+
         def count_allstar(hule: dict) -> int:
-            for fans in hule['fans']:
-                if fans['id'] == 32:
-                    if fans['val'] == 3:
+            for fans in hule["fans"]:
+                if fans["id"] == 32:
+                    if fans["val"] == 3:
                         return 1
-            return 0 
-        
+            return 0
+
         def count_yiman(hule: dict) -> int:
-            if hule['yiman']:
+            if hule["yiman"]:
                 return 1
             return 0
-                
+
         def count_ura_dora(hule: dict) -> int:
-            for fans in hule['fans']:
-                if fans['id'] == 33:
-                    return fans['val']
+            for fans in hule["fans"]:
+                if fans["id"] == 33:
+                    return fans["val"]
             return 0
-        
-        def fangchong_user(delta_scores: list[int]) -> User:
+
+        def fangchong_user(delta_scores: list[int]) -> User | None:
             for i, score in enumerate(delta_scores):
                 if score < 0:
                     return self._get_user_with_seat(i)
             return None
-                
-        def tobi_users(scores: list[int]) -> list[User]:
+
+        def tobi_users(scores: list[int]) -> list[User] | None:
             users = []
             for i, score in enumerate(scores):
                 if score < 0:
@@ -115,21 +118,24 @@ class Jansoul:
 
             if len(users) == 0:
                 return None
-            
+
             return users
 
-        def count(hule: dict, fangchong: User = None, tobi: list[User] = None):
-            winner = self._get_user_with_seat(hule['seat'])
+        def count(hule: dict, fangchong: User | None = None, tobi: list[User] | None = None):
+            winner = self._get_user_with_seat(hule["seat"])
             ippatsu = count_ippatsu(hule)
             ura_dora = count_ura_dora(hule)
             aka_dora = count_aka_dora(hule)
             yiman = count_yiman(hule)
             allstar = count_allstar(hule)
 
+            if winner is None:
+                return
+
             if ippatsu + ura_dora + aka_dora + yiman + allstar == 0:
                 return
 
-            if fangchong is None: #ツモ
+            if fangchong is None:  # ツモ
                 winner.ippatsu_tumo += ippatsu
                 winner.ura_dora_tumo += ura_dora
                 winner.aka_dora_tumo += aka_dora
@@ -137,43 +143,56 @@ class Jansoul:
                 winner.allstar_tumo += allstar
 
                 for user in self.users:
-                    if not user.seat == hule['seat']:
+                    if not user.seat == hule["seat"]:
                         if ippatsu > 0:
-                            user.transaction.append(Hule(to=winner.nickname, cnt=ippatsu, bonus=Bonus.ippatsu, zimo=True))            
+                            user.transaction.append(
+                                Hule(to=winner.nickname, cnt=ippatsu, bonus=Bonus.ippatsu, zimo=True)
+                            )
                         if ura_dora > 0:
-                            user.transaction.append(Hule(to=winner.nickname, cnt=ura_dora, bonus=Bonus.ura_dora, zimo=True))
+                            user.transaction.append(
+                                Hule(to=winner.nickname, cnt=ura_dora, bonus=Bonus.ura_dora, zimo=True)
+                            )
                         if aka_dora > 0:
-                            user.transaction.append(Hule(to=winner.nickname, cnt=aka_dora, bonus=Bonus.aka_dora, zimo=True))     
+                            user.transaction.append(
+                                Hule(to=winner.nickname, cnt=aka_dora, bonus=Bonus.aka_dora, zimo=True)
+                            )
                         if yiman > 0:
-                            user.transaction.append(Hule(to=winner.nickname, cnt=yiman, bonus=Bonus.yiman, zimo=True))    
+                            user.transaction.append(Hule(to=winner.nickname, cnt=yiman, bonus=Bonus.yiman, zimo=True))
                         if allstar > 0:
-                            user.transaction.append(Hule(to=winner.nickname, cnt=allstar, bonus=Bonus.allstar, zimo=True))
-                            
-            else: #ロン
+                            user.transaction.append(
+                                Hule(to=winner.nickname, cnt=allstar, bonus=Bonus.allstar, zimo=True)
+                            )
+
+            else:  # ロン
                 winner.ippatsu += ippatsu
                 winner.ura_dora += ura_dora
                 winner.aka_dora += aka_dora
                 winner.yiman += yiman
                 winner.allstar += allstar
                 if ippatsu > 0:
-                    fangchong.transaction.append(Hule(to=winner.nickname, cnt=ippatsu, bonus=Bonus.ippatsu, zimo=False))                    
+                    fangchong.transaction.append(Hule(to=winner.nickname, cnt=ippatsu, bonus=Bonus.ippatsu, zimo=False))
                 if ura_dora > 0:
-                    fangchong.transaction.append(Hule(to=winner.nickname, cnt=ura_dora, bonus=Bonus.ura_dora, zimo=False))                   
+                    fangchong.transaction.append(
+                        Hule(to=winner.nickname, cnt=ura_dora, bonus=Bonus.ura_dora, zimo=False)
+                    )
                 if aka_dora > 0:
-                    fangchong.transaction.append(Hule(to=winner.nickname, cnt=aka_dora, bonus=Bonus.aka_dora, zimo=False))                   
+                    fangchong.transaction.append(
+                        Hule(to=winner.nickname, cnt=aka_dora, bonus=Bonus.aka_dora, zimo=False)
+                    )
                 if yiman > 0:
-                    fangchong.transaction.append(Hule(to=winner.nickname, cnt=yiman, bonus=Bonus.yiman, zimo=False))                   
+                    fangchong.transaction.append(Hule(to=winner.nickname, cnt=yiman, bonus=Bonus.yiman, zimo=False))
                 if allstar > 0:
-                    fangchong.transaction.append(Hule(to=winner.nickname, cnt=allstar, bonus=Bonus.allstar, zimo=False))                   
-            
+                    fangchong.transaction.append(Hule(to=winner.nickname, cnt=allstar, bonus=Bonus.allstar, zimo=False))
+
             if tobi is not None:
                 winner.tobi += len(tobi)
-                for user in tobi: #トビ
+                for user in tobi:  # トビ
                     user.transaction.append(Hule(to=winner.nickname, cnt=1, bonus=Bonus.tobi, zimo=fangchong is None))
 
         recordHule = self._get_recordHule()
         for record in recordHule:
-            for hule in record['result']['data']['hules']:
-                user = None if hule['zimo'] == True else fangchong_user(record['result']['data']['delta_scores'])
-                tobi = tobi_users(record['result']['data']['scores'])
+            for hule in record["result"]["data"]["hules"]:
+                user = None if hule["zimo"] == True else fangchong_user(record["result"]["data"]["delta_scores"])
+                tobi = tobi_users(record["result"]["data"]["scores"])
+                count(hule, user, tobi)
                 count(hule, user, tobi)
